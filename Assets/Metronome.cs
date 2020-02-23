10e78e8fcc42b3044d6c829f551725975fa21674
FrancescoXX <francescociullaroma@gmail.com>
2020-02-23 21:39:27 +0100

    initial commit

---
+++ Assets/Metronome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+//SRP: Calls periodic configurable Events
+public class Metronome : MonoBehaviour {
+
+    CompositeDisposable disposables = new CompositeDisposable();
+
+    public void EnableMetronome() {
+        print("Metronome Activated!");
+        disposables.Clear();
+
+        Observable
+            .Interval(TimeSpan.FromSeconds(Random.Range(1, 2)))
+            .Subscribe(x => {
+                //Call Event 1 happened...
+                Debug.Log("Timed Event 1");
+            })
+            .AddTo(disposables);
+
+        Observable
+            .Interval(TimeSpan.FromSeconds(Random.Range(1, 5)))
+            .Subscribe(x => {
+                //Call Event 2 happened...
+                Debug.Log("Timed Event 2");
+            })
+            .AddTo(disposables);
+    }
+
+    public void DisableMetronome() {
+        print("Metronome Disabled!");
+        disposables.Clear();
+    }
+
+    private void OnDisable() => disposables.Clear();
+}
